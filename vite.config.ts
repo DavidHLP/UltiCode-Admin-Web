a614c2213b8077e3eef48fc0f3708cb86c7f180a
DavidHLP <lysf15520112973@163.com>
2025-10-14 23:56:27 +0800

    chore: 更新项目依赖和配置- 在 admin-console 中添加 @types/node依赖 -重构 admin-console 的 TypeScript 配置文件结构 - 调整 Vite 配置中的导入语句格式- 更新 auth 和 gateway服务的日志级别配置 - 修改 gateway 路由 ID 并调整 JwtAuthenticationFilter 代码格式- 创建 common 模块及其子模块 forward - 从 gateway 模块中移除 WebFlux 和 Security 依赖- 在根 pom.xml 中添加 common 模块引用

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
+import {fileURLToPath, URL} from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 // 导入 Vue Devtools 插件
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
-    const { PrimeVueResolver } = await import('@primevue/auto-import-resolver');
+    const {PrimeVueResolver} = await import('@primevue/auto-import-resolver');
 
     return {
         optimizeDeps: {
